7b00f5c9217d68eeb44f431c25132c5a4c969464
Wellynton Diniz <wellyntondiniz@gmail.com>
2025-05-26 20:03:14 -0400

    v1

--- enquete/urls.py
+++ enquete/urls.py
@@ -4,5 +4,7 @@ from django.urls import path
 app_name = 'enquete'
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<int:id_questao>/votar', views.votar, name='votar'),
+    path('<int:id_questao>/resultados', views.resultados, name='resultados'),
 ]
--- enquete/views.py
+++ enquete/views.py
@@ -1,12 +1,32 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 from enquete.models import Questao
+
 
 def index(request):
     questoes = Questao.objects.order_by('descricao')
     contexto = {'questoes': questoes}
     return render(request, 'enquete/index.html', contexto)
+
+def resultados(request, id_questao):
+    question = Questao(pk=id_questao)
+    context = {'question': question}
+    return render(request, 'enquete/resultados.html', context)
+
+def votar(request, id_questao):
+    questao = get_object_or_404(Questao, pk=id_questao)
+    try:
+        selected_option = questao.alternativa_set.get(pk=request.POST['option'])
+    except KeyError:
+        return render(request, 'enquete/voto.html', {
+            'questao': questao,
+            'error_message': "Selecione uma opção",
+        })
+    else:
+        selected_option.votos += 1
+        selected_option.save()
+        return HttpResponseRedirect(reverse('enquete:resultados', args=(id_questao,)))
 
 
 
